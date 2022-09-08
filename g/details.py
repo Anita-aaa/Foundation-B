b0caf01d3ee983121d582da39585b693dfa20e1d
Anita-aaa <at9941787@gmail.com>
2022-09-08 21:20:18 +0545

    first commit

---
+++ g/details.py
@@ -0,0 +1,9 @@
+# name= input("Anita")
+# age= input("19")
+# print(f"Hi! your name is {Anita}and your age is {19}years old)
+
+
+#college_name = input("Softwarica")
+# classroom = input("LR15")
+# print(f"Hi! your college name is {Softwarica}and your classroom is {LR15} room no")
+
