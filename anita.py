25bc779c669623d0071c6e20a6309212b93836fb
Anita-aaa <at9941787@gmail.com>
2022-09-14 00:03:53 +0545

    first commit

--- anita.py
+++ anita.py
@@ -1,1 +1,17 @@
-age = int("please provide your age")
+age = int(input("please provide your age"))
+
+#conditional statement
+#if block only
+
+if age>18:
+    print("you can play this game because you are above 18")
+else:
+    print("you need to be above 18 years old")
+
+# proper use of if-elif-else block
+if 0<age<10:
+    print ("so you are kid,your movie ticket is Nrs 100")
+elif 10<age<20:
+    print("your movie ticket is Nrs. 200")
+else:
+    print("your movie ticket is Nrs 300")
--- coading.py
+++ coading.py
@@ -1,1 +1,5 @@
-print("hello world")
+list=(5,7,3)
+print(15 not in list)
+if 15 not in list:
+    print("its not in the list")
+    
